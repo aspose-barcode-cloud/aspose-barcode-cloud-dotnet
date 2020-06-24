1579908564097aa001b9a2b9241afd39ac1325af
Denis Averin <denis.averin@aspose.com>
2020-06-24 19:05:46 +0700

    Allow build package only from master

--- build_package.cmd
+++ build_package.cmd
@@ -1,11 +1,9 @@
 REM Check state
+git fetch origin || EXIT /B 1
 git diff origin/master --exit-code || (
     echo Unmerged state
     EXIT /B 1
 )
-
-REM Clean working copy
-git clean -dfx --exclude=Tests/Configuration*.json || EXIT /B 1
 
 REM Build and pack
 msbuild -restore -t:clean,rebuild,pack -p:Configuration=Release || EXIT /B 1
