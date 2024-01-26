d4f8ed3c0328c2bcce9d26933f8b1e108aee06a6
Denis Averin <denis.averin@aspose.com>
2024-01-26 19:31:34 +0700

    Build examples with new NuGet package

--- Makefile
+++ Makefile
@@ -35,7 +35,6 @@ update:
 .PHONY: lint
 lint:
 	dotnet build --warnaserror Aspose.BarCode.Cloud.Sdk.sln
-	dotnet build --warnaserror examples/examples.sln
 
 .PHONY: clean
 clean:
--- scripts/pack-nuget.bash
+++ scripts/pack-nuget.bash
@@ -2,9 +2,9 @@
 set -euo pipefail
 
 if [ $# -ge 1 ]; then
-    output_dir="$1"
+    output_dir="$(realpath "$1")"
 else
-    output_dir=".."
+    output_dir="$(realpath ..)"
 fi
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
@@ -13,5 +13,6 @@ SRC_DIR="$( cd "${SCRIPT_DIR}/../src" &> /dev/null && pwd )"
 pushd "${SRC_DIR}"
 export RELEASE_BUILD=true
 dotnet build --warnaserror --configuration=Release Aspose.BarCode.Cloud.Sdk.csproj
+echo "Packing nuget package to \"${output_dir}\" ..."
 dotnet pack --configuration=Release Aspose.BarCode.Cloud.Sdk.csproj --output="${output_dir}"
 popd
