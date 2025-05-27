7758f46c4b2ecf2b8342199800a99c58528749ec
ivankamkin <109517020+ivankamkin@users.noreply.github.com>
2025-05-27 17:38:28 +0500

    Release 25.5 (#106)

--- src/Model/BarcodeImageParams.cs
+++ src/Model/BarcodeImageParams.cs
@@ -28,32 +28,32 @@ namespace Aspose.BarCode.Cloud.Sdk.Model
         public GraphicsUnit? Units { get; set; }
 
         /// <summary>
-        ///     Specify the displaying bars and content Color.  Value: Color name from https://reference.aspose.com/drawing/net/system.drawing/color/ or ARGB value started with #.  For example: AliceBlue or #FF000000  Default value: Black.
+        ///     Specify the displaying bars and content Color. Value: Color name from https://reference.aspose.com/drawing/net/system.drawing/color/ or ARGB value started with #. For example: AliceBlue or #FF000000 Default value: Black.
         /// </summary>
         public string ForegroundColor { get; set; }
 
         /// <summary>
-        ///     Background color of the barcode image.  Value: Color name from https://reference.aspose.com/drawing/net/system.drawing/color/ or ARGB value started with #.  For example: AliceBlue or #FF000000  Default value: White.
+        ///     Background color of the barcode image. Value: Color name from https://reference.aspose.com/drawing/net/system.drawing/color/ or ARGB value started with #. For example: AliceBlue or #FF000000 Default value: White.
         /// </summary>
         public string BackgroundColor { get; set; }
 
         /// <summary>
-        ///     Resolution of the BarCode image.  One value for both dimensions.  Default value: 96 dpi.  Decimal separator is dot.
+        ///     Resolution of the BarCode image. One value for both dimensions. Default value: 96 dpi. Decimal separator is dot.
         /// </summary>
         public float? Resolution { get; set; }
 
         /// <summary>
-        ///     Height of the barcode image in given units. Default units: pixel.  Decimal separator is dot.
+        ///     Height of the barcode image in given units. Default units: pixel. Decimal separator is dot.
         /// </summary>
         public float? ImageHeight { get; set; }
 
         /// <summary>
-        ///     Width of the barcode image in given units. Default units: pixel.  Decimal separator is dot.
+        ///     Width of the barcode image in given units. Default units: pixel. Decimal separator is dot.
         /// </summary>
         public float? ImageWidth { get; set; }
 
         /// <summary>
-        ///     BarCode image rotation angle, measured in degree, e.g. RotationAngle = 0 or RotationAngle = 360 means no rotation.  If RotationAngle NOT equal to 90, 180, 270 or 0, it may increase the difficulty for the scanner to read the image.  Default value: 0.
+        ///     BarCode image rotation angle, measured in degree, e.g. RotationAngle = 0 or RotationAngle = 360 means no rotation. If RotationAngle NOT equal to 90, 180, 270 or 0, it may increase the difficulty for the scanner to read the image. Default value: 0.
         /// </summary>
         public int? RotationAngle { get; set; }
 
