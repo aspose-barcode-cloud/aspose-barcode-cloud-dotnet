10534b9e83b28f67668ad3481880ef9bdb7374e8
Denis Averin <denis.averin@aspose.com>
2026-07-07 18:21:56 +0700

    Accept v4 branch in badge branch check

--- scripts/check-badges.bash
+++ scripts/check-badges.bash
@@ -16,7 +16,7 @@ pushd "${SCRIPT_DIR}/.." >/dev/null
     fi
 done
 
-(grep -oP '[^/]+\.yml/badge.svg(?!\?branch=main)' "${readme_file}" || echo ) | while read -r badge_without_branch; do
+(grep -oP '[^/]+\.yml/badge.svg(?!\?branch=(main|v4)\b)' "${readme_file}" || echo ) | while read -r badge_without_branch; do
     if [ -z "${badge_without_branch}" ]; then continue; fi
     >&2 echo "Badge without branch \"${badge_without_branch}\""
     exit 1
